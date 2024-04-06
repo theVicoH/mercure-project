ff4e725532cafe8b124780f14689392a6327fe6a
Victor <victor.huang@shadow.tech>
2024-04-07 01:35:44 +0200

    Change ports structure

--- backend/src/ports/adapters/controllers/IUserController.ts
+++ backend/src/ports/adapters/controllers/IUserController.ts
@@ -1,4 +1,4 @@
-import { ResponseController } from '../../types/Response';
+import { ResponseController } from '../../../types/Response';
 
 export default interface IUserController {
   register: (username: string, password: string) => Promise<ResponseController>;
---
+++ backend/src/ports/adapters/repositories/IUserRepository.ts
@@ -0,0 +1,6 @@
+import { User } from '../../../entities/UserEntities';
+
+export default interface IUserRepository {
+  createUser: (username: string, password: string) => Promise<User>;
+  findUser: (username: string, password: string) => Promise<User>;
+}
