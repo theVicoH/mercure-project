4acdf628bf2b5a66e53efc0f3c925be0aba31ad2
theVicoH <huang.victor.mail@gmail.com>
2024-04-07 23:28:27 +0200

    Delete ports interface for useCase

--- backend/src/ports/useCases/IUserUseCase.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../../entities/UserEntities';
-
-export default interface IUserUseCase {
-  createUser: (username: string, password: string) => Promise<User>;
-  findUser: (username: string, password: string) => Promise<string>;
-}
