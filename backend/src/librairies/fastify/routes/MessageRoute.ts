833155465a24cdf5c806a06d8e31b08ca006fbdc
theVicoH <huang.victor.mail@gmail.com>
2024-04-07 19:19:19 +0200

    Create message routes

---
+++ backend/src/librairies/fastify/routes/MessageRoute.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify';
+import { PrivateRoutes } from '../../../routes/routes';
+import auth from '../middlewares/Auth';
+
+interface MessageRequestBody {
+  conversationId: number;
+  message: string;
+}
+
+export async function messageRoutes(fastify: FastifyInstance) {
+  fastify.post<{ Body: MessageRequestBody }>(
+    PrivateRoutes.SendMessage,
+    { preHandler: auth },
+    async (request, reply) => {
+      const { conversationId, message } = request.body;
+      if (!request.user) {
+        return reply
+          .code(401)
+          .send({ error: 'Unauthorized: User ID is missing from the request' });
+      }
+      // const result = await friendController.addFriend(
+      //   request.user.userId,
+      //   friendUsername
+      // );
+      // reply.code(result.code).send(result.body);
+    }
+  );
+}
