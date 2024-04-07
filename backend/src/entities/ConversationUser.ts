c543d1cb9218b3dfa7d3dc6c16abb8d018e26f6e
theVicoH <huang.victor.mail@gmail.com>
2024-04-07 19:05:05 +0200

    Create ConversationUser entities

---
+++ backend/src/entities/ConversationUser.ts
@@ -0,0 +1,12 @@
+class ConversationUser {
+  conversationId: number;
+  userId: number;
+
+  constructor(conversationId: number, userId: number) {
+    this.conversationId = conversationId;
+    this.userId = userId;
+  }
+
+}
+
+export default ConversationUser;
