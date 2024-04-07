b6a06b8ed6a90bec71ac36344f3253ebb02cf568
theVicoH <huang.victor.mail@gmail.com>
2024-04-07 23:58:23 +0200

    Add transactions addFriend

--- backend/src/librairies/db/transactions/Transactions.ts
+++ backend/src/librairies/db/transactions/Transactions.ts
@@ -1,23 +1,36 @@
 import { Friend } from '../../../entities/FriendEntities';
-import ITransactions from '../../../ports/librairies/db/transactions/ITransactions';
+import { IServicesPack, ITransactions } from '../../../types/IServices';
+import sequelize from '../Sequalize';
 
 export default class Transactions implements ITransactions {
-  async addFriend(userId: number, friendUsername: string) : Promise<Friend> {
-    const friendFound =
-      await this.services.userService.findUser(friendUsername);
-    if (!friendFound) {
-      throw new Error("This user don' exist");
-    }
-    const friendId = friendFound.id;
+  constructor(private services: IServicesPack) {}
+  async addFriend(userId: number, friendUsername: string) {
+    const transaction = await sequelize.transaction();
+    try {
+      const friendFound =
+        await this.services.userService.findUser(friendUsername);
+      if (!friendFound) {
+        throw new Error("This user don' exist");
+      }
+      const friendId = friendFound.id;
 
-    const exists = await this.services.friendService.checkFriendship(
-      userId,
-      friendId
-    );
-    if (exists) {
-      throw new Error(`You are already friend with ${friendUsername}`);
-    }
-    await this.services.friendService.createFriendConnection(userId, friendId);
+      const exists = await this.services.friendService.checkFriendship(
+        userId,
+        friendId
+      );
+      if (exists) {
+        throw new Error(`You are already friend with ${friendUsername}`);
+      }
+      await this.services.friendService.createFriendConnection(userId, friendId);
+      const conversation = await this.services.conversationService.createConversation(transaction);
 
+      await this.services.conversationUserService.createConversationUser(conversation.id, userId, transaction);
+      await this.services.conversationUserService.createConversationUser(conversation.id, friendId, transaction);
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+    
   }
 }
