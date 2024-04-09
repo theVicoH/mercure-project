8a5f8b7f4a280ba69b0d5aa0bc1157ac6c1d36da
theVicoH <huang.victor.mail@gmail.com>
2024-04-09 19:00:30 +0200

    sendMessage in MessageController

--- backend/src/controllers/MessageController.ts
+++ backend/src/controllers/MessageController.ts
@@ -1,5 +1,22 @@
 import { IMessageController } from '../types/IControllers';
+import { IMessageUseCase } from '../types/IUseCases';
 
 export class MessageController implements IMessageController {
-  constructor() {}
+  constructor(private messageUseCase: IMessageUseCase) {}
+
+  async sendMessage(conversationId: number, senderId: number, message: string) {
+    try {
+      const messageData = await this.messageUseCase.createMessage(conversationId, senderId, message);
+      return {
+        code: 201,
+        body: { message: 'Message successfully send', data: messageData },
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        return { code: 500, body: { message: error.message } };
+      } else {
+        return { code: 500, body: { message: 'An unknown error occurred' } };
+      }
+    }
+  };
 }
