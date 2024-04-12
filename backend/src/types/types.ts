70b5b09a9b5433a918d621990eaf64f9f0aed8df
theVicoH <huang.victor.mail@gmail.com>
2024-04-13 01:29:33 +0200

    Conversation Custom types

---
+++ backend/src/types/types.ts
@@ -0,0 +1,7 @@
+export interface ConversationCustomTypes {
+  id: number;
+  friendUsername: string | null;
+  message: string | null;
+  messageSentAt: Date | null;
+  lastMessageRead: boolean | null;
+}
