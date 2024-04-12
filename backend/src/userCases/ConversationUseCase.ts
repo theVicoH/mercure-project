3e366cd6509c875d2aebc1f92ce8e30088a57877
theVicoH <huang.victor.mail@gmail.com>
2024-04-13 01:31:12 +0200

    Conversation use case

--- backend/src/userCases/ConversationUseCase.ts
+++ backend/src/userCases/ConversationUseCase.ts
@@ -1,5 +1,22 @@
 import { IConversationUseCase, IUseCasesConstructor } from '../types/IUseCases';
+import { ConversationCustomTypes } from '../types/types';
 
 export class ConversationUseCase implements IConversationUseCase {
   constructor(private services: IUseCasesConstructor) {}
+
+  public async findUserConversations(
+    userId: number
+  ): Promise<ConversationCustomTypes[]> {
+    console.log(false);
+
+    const conversations =
+      await this.services.conversationService.findConversationsByUserId(userId);
+    console.log(conversations);
+
+    if (!conversations) {
+      throw new Error('No conversations found');
+    }
+    console.log(conversations);
+    return conversations;
+  }
 }
