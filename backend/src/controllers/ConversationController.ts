fe8f79ea00d95c7144826ccf26843c6024a9071a
theVicoH <huang.victor.mail@gmail.com>
2024-04-13 01:31:24 +0200

    Conversation controller

--- backend/src/controllers/ConversationController.ts
+++ backend/src/controllers/ConversationController.ts
@@ -1,5 +1,28 @@
 import { IConversationController } from '../types/IControllers';
+import { IConversationUseCase } from '../types/IUseCases';
+import { ResponseController } from '../types/Response';
+import { ConversationCustomTypes } from '../types/types';
 
 export class ConversationController implements IConversationController {
-  constructor() {}
+  constructor(private conversationUseCase: IConversationUseCase) {}
+
+  public async userConversationsList(
+    userId: number
+  ): Promise<ResponseController<ConversationCustomTypes[]>> {
+    try {
+      const conversation =
+        await this.conversationUseCase.findUserConversations(userId);
+
+      return {
+        code: 201,
+        body: { message: 'User conversation list found', data: conversation },
+      };
+    } catch (error) {
+      if (error instanceof Error) {
+        return { code: 500, body: { message: error.message } };
+      } else {
+        return { code: 500, body: { message: 'An unknown error occurred' } };
+      }
+    }
+  }
 }
