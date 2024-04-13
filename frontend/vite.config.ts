73890763229ae181b7dac0ecb5584772dcfbfb9b
theVicoH <huang.victor.mail@gmail.com>
2024-04-14 01:26:36 +0200

    Dockerise front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' });
 
 export default defineConfig({
   plugins: [react()],
