79c39532043f8f6e1da18e40ebd88f83acf75abb
theVicoH <huang.victor.mail@gmail.com>
2024-04-13 22:22:20 +0200

    Install react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 function App() {
-
+  const queryClient = new QueryClient();
   return (
-    <>
-    </>
+    <QueryClientProvider client={queryClient}>
+
+    </QueryClientProvider>
   )
 }
 
