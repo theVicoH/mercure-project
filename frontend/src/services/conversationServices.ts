06147095f81a4e584d92b586ce729ded984ff9a9
theVicoH <huang.victor.mail@gmail.com>
2024-04-14 18:47:24 +0200

    Create conversationService

---
+++ frontend/src/services/conversationServices.ts
@@ -0,0 +1,25 @@
+import { ApiResponse, ConversationListResponse } from "@/types/response";
+
+const URL = import.meta.env.VITE_REACT_APP_API_URL;
+
+
+export const conversationListService = async (authToken: string): Promise<ApiResponse<ConversationListResponse[]>> => {
+  try {
+    const response = await fetch(`${URL}/conversation-list`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const responseData = await response.json();
+
+    return responseData;
+  } catch {
+    throw new Error('Error when trying to retrieve conversation list');
+  }
+};
