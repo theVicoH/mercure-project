da4726bc6d353af6195999a98270afd477047501
djaout lyes <djaoutlyes24@gmail.com>
2024-04-14 02:32:22 +0200

    feat: Implemented findFriendsByUserId method in FrindUseCase to retrieve friends information using userService

--- backend/src/userCases/FriendUseCase.ts
+++ backend/src/userCases/FriendUseCase.ts
@@ -1,5 +1,5 @@
 import { Friend } from '../entities/FriendEntities';
-import { IFriendUseCase, IUseCasesConstructor } from '../types/IUseCases';
+import { IFriendUseCase, IUseCasesConstructor, IUserInfo } from '../types/IUseCases';
 
 export class FriendUseCase implements IFriendUseCase {
   constructor(private services: IUseCasesConstructor) {}
@@ -65,5 +65,29 @@ export class FriendUseCase implements IFriendUseCase {
       await transaction.rollback();
       throw error;
     }
+  }
+
+  public async findFriendsByUserId(userId: number): Promise<IUserInfo[]> {
+    const friendsFound = await this.services.friendService.findFriendsByUserId(userId);  
+
+    if (!friendsFound || friendsFound.length === 0 || friendsFound === null) {
+      throw new Error('Friends not found');
+    }
+    
+    const friendsInfoPromises: Promise<IUserInfo>[] = friendsFound.map(async (friend) => {
+    
+      const friendUser = await this.services.userService.findUserById(friend.friendId);
+      
+      return {
+        id: friendUser.id,
+        username: friendUser.username,
+        photo: friendUser.photo,
+        createdAt: friend.createdAt,
+      };
+    });
+
+    const friendsInfo = await Promise.all(friendsInfoPromises);
+
+    return friendsInfo;
   }
 }
