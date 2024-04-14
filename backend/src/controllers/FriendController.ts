a963dd0631a0fbe0dcefd3d043b72c506da2d38b
djaout lyes <djaoutlyes24@gmail.com>
2024-04-14 02:30:37 +0200

    feat: add getAllFriends method in FriendController to get all friends…

--- backend/src/controllers/FriendController.ts
+++ backend/src/controllers/FriendController.ts
@@ -1,5 +1,5 @@
 import { IFriendController } from '../types/IControllers';
-import { IFriendUseCase } from '../types/IUseCases';
+import { IFriendUseCase, IUserInfo } from '../types/IUseCases';
 import { ResponseController } from '../types/Response';
 
 export class FriendController implements IFriendController {
@@ -23,5 +23,20 @@ export class FriendController implements IFriendController {
         return { code: 500, body: { message: 'An unknown error occurred' } };
       }
     }
+  }
+
+  public async getAllFriends(userId: number) : Promise<ResponseController<IUserInfo[]>> {
+    try {    
+      const friendsInfo = await this.friendUseCase.findFriendsByUserId(userId);
+
+      return { code: 200, body: { message: 'Friends informations retrieved successfully', data: friendsInfo } };
+      
+    } catch (error) {
+        if (error instanceof Error && error.message === 'Friends not found') {
+          return { code: 404, body: { message: error.message } };
+        } else {
+          return { code: 500, body: { message: 'An unknown error occurred' } };
+        }
+      }
   }
 }
