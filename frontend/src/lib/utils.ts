6ebf85972d6c83bcb485b330a0b333eb9815f639
theVicoH <huang.victor.mail@gmail.com>
2024-04-14 22:38:24 +0200

    Create isSuccessResponse utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,11 @@
+import { ApiResponse, SuccessResponse } from '@/types/response';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function isSuccessResponse<T>(response: ApiResponse<T>): response is SuccessResponse<T> {
+  return 'body' in response && 'data' in response.body;
 }
