cb12e17555572469abad2e2b9d6d18df524d0c71
theVicoH <huang.victor.mail@gmail.com>
2024-04-14 17:08:00 +0200

    Create login service

--- frontend/src/services/authServices.ts
+++ frontend/src/services/authServices.ts
@@ -1,13 +1,14 @@
-import { ApiResponse } from '@/types/response';
-import { registerSchema } from '@/types/zod/authForm';
+import { ApiResponse, LoginResponse } from '@/types/response';
+import { loginSchema, registerSchema } from '@/types/zod/authForm';
 import { z } from 'zod';
+
+const URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 export const registerService = async (
   data: z.infer<typeof registerSchema>
 ): Promise<ApiResponse> => {
   try {
-    const url = import.meta.env.VITE_REACT_APP_API_URL;
-    const response = await fetch(`${url}/register`, {
+    const response = await fetch(`${URL}/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -18,5 +19,23 @@ export const registerService = async (
     return responseData;
   } catch {
     throw new Error('Error when trying to register');
+  }
+};
+
+export const loginService = async (
+  data: z.infer<typeof loginSchema>
+): Promise<ApiResponse<LoginResponse>> => {
+  try {
+    const response = await fetch(`${URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.json();
+    return responseData;
+  } catch {
+    throw new Error('Error when trying to login');
   }
 };
