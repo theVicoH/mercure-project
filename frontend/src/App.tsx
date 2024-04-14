3bce01e50a81a795526c8f4b6217b32d9516da6b
theVicoH <huang.victor.mail@gmail.com>
2024-04-14 17:43:14 +0200

    Create test page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
-import ConversationPage from "./pages/conversation";
+import ChatPage from "./pages/chat";
 import { RoutesPath } from './types/routes';
 import ToasterProvider from './features/toaster/toasterProviders';
 import { useSelector } from 'react-redux';
 import { RootState } from './stores/store';
+import Test from './pages/test';
 
 function App() {
   const authToken = useSelector((state: RootState) => state.auth.jwt);
@@ -14,9 +15,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Navigate replace to={RoutesPath.Login} />} />
-          {!authToken ? <Route path={RoutesPath.Login} element={<LoginPage />} /> : <Route path={RoutesPath.Login} element={<Navigate to={RoutesPath.Conversation} />} />}
-          {!authToken ? <Route path={RoutesPath.Register} element={<RegisterPage />} /> : <Route path={RoutesPath.Register} element={<Navigate to={RoutesPath.Conversation} />} />}
-          {authToken && <Route path={RoutesPath.Conversation} element={<ConversationPage />} /> }
+          {!authToken ? <Route path={RoutesPath.Login} element={<LoginPage />} /> : <Route path={RoutesPath.Login} element={<Navigate to={RoutesPath.Chat} />} />}
+          {!authToken ? <Route path={RoutesPath.Register} element={<RegisterPage />} /> : <Route path={RoutesPath.Register} element={<Navigate to={RoutesPath.Chat} />} />}
+          {authToken && <Route path={RoutesPath.Chat} element={<ChatPage />} /> }
+          {authToken && <Route path={RoutesPath.Test} element={<Test />} /> }
         </Routes>
       </Router>
     </ToasterProvider>
