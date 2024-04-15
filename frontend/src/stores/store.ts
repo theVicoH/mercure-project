4b05c33fb696f825bc130757933ceb654fe2c2e8
theVicoH <huang.victor.mail@gmail.com>
2024-04-15 20:00:05 +0200

    Fix auth slice

--- frontend/src/stores/store.ts
+++ frontend/src/stores/store.ts
@@ -3,33 +3,23 @@ import authReducer from './slice/auth';
 import toasterNotifReducer from './slice/toasterNotif';
 import userProfilReducer from './slice/userProfil';
 import currentConversationReducer from './slice/currentConversation';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['auth'],
-};
-
-const rootReducer = {
-  auth: persistReducer(persistConfig, authReducer),
-  showToaster: toasterNotifReducer,
-  userProfil: userProfilReducer,
-  currentConversation: currentConversationReducer,
-};
 
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
+  reducer: {
+    auth: authReducer,
+    showToaster: toasterNotifReducer,
+    userProfil: userProfilReducer,
+    currentConversation: currentConversationReducer,
+  },
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    });
+  },
 });
-
-const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export { store, persistor };
+export default store;
