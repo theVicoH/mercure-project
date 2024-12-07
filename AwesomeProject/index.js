f87e7871c24fa6bff846afd20027d79e095ee4bb
Victor Huang <huang.victor.mail@gmail.com>
2024-12-07 12:41:30 +0100

    Delete React native project

--- AwesomeProject/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import App from './App'; // Adjust the path as necessary
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
